d1977c3a68b6cd00385e48ab5f7dfec28fc4c8ba
Richard Sanchez <richard.charles.s@gmail.com>
2019-01-02 22:31:51 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get "/signin", to: "sessions#new"
   post "/signin", to: "sessions#create"
   get "/signout", to: "sessions#destroy"
-  delete "/signout", to: "sessions#destroy"
 
   post "/rides/new", to: "rides#new"
 
