a868fb1b2b32e7a8c7a6102d1c8b0affedab6b65
Richard Sanchez <richard.charles.s@gmail.com>
2019-01-02 22:17:42 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   validates :name, presence: true
 
   has_many :rides
-  has_many :attractions, :through => :rides
+  has_many :attractions, through: :rides
 
   def mood
     if self.happiness && self.nausea
