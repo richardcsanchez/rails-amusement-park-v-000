94b1ed6bf52d46ab22ac5d16a371f18f91d791e1
Richard Sanchez <richard.charles.s@gmail.com>
2019-01-02 23:00:02 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   skip_before_action :require_logged_in, only: [:new, :create]
 
   def new
-    @user = User.new
+    @user = User.find_by_id(:user_id)
   end
 
   def create
