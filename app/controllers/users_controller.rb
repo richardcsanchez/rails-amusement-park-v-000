474a1849ab24fb2fa25e608352b992e5d4e9c0e8
Richard Sanchez <richard.charles.s@gmail.com>
2019-01-02 22:34:58 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-     if @user.save
+     if @user
+       @user.save
        session[:user_id] = @user.id
        redirect_to @user
      else
